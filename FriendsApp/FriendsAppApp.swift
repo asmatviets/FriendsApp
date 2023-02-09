1dce7ef9ddf272cfe612eed969ec4040fba5f4ed
AndreyMatviets <asmatviets@gmail.com>
2023-02-09 12:11:28 +0400

    Initial Commit

---
+++ FriendsApp/FriendsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppApp.swift
+//  FriendsApp
+//
+//  Created by Andrey Matviets on 09.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
